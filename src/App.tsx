6306d848b7846cad59c7bcdd88cbd66e785daca7
Vinicius Barbosa <94053437+Vinicius-Barbosa-de-Oliveira@users.noreply.github.com>
2024-04-01 15:52:49 -0300

    correções leves de palavras

--- src/App.tsx
+++ src/App.tsx
@@ -3,190 +3,6 @@ import axios, { AxiosResponse } from "axios";
 import "./App.css";
 
 
-/*ype Usuario = {
-  id: number,
-  name: string,
-  username: string,
-  email: string,
-  address: object,
-  phone: string,
-  website: string,
-  company: object;
-};
-
-type Publicacao = {
-  id: number,
-  titulo: string,
-  body: string;
-};
-
-type Album = {
-  id: number,
-  titulo: string;
-};
-
-const ListaDeTarefas = () => {
-  const [tarefas, setTarefas] = useState([]);
-  const escutarCliqueAcessarAPI = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos").then((resposta: AxiosResponse) => {
-      const dados = resposta.data.map((item: { id: number; title: string; completed: boolean; }) => {
-        return {
-          id: item.id,
-          titulo: item.title,
-          concluido: item.completed,
-        };
-      });
-      setTarefas(dados);
-    });
-  };
-  
-  return (
-    <>
-      <h4>Tarefas</h4>
-      <div>
-        <button onClick={escutarCliqueAcessarAPI}>Atualizar lista de tarefas</button>
-      </div>
-      <ul>
-        {
-          tarefas.map((item: Tarefa) => {
-            return <ItemTarefa key={item.id} titulo={item.titulo} />
-          })
-        }
-      </ul>
-    </>
-  );
-}
-
-const ItemTarefa = (props: {titulo: string}) => {
-  return (<li>{props.titulo}</li>);
-}
-
-const ListaDePublicacoes = () => {
-  const [publicacoes, setPublicacoes] = useState([]);
-  const escutarCliqueAcessarAPI = () => {
-    axios.get("https://jsonplaceholder.typicode.com/posts/").then((resposta: AxiosResponse) => {
-      const posts = resposta.data.map((post: { id: number; title: string; body: string; }) => {
-        return {
-          id: post.id,
-          titulo: post.title,
-          conteudo: post.body,
-        };
-      });
-      setPublicacoes(posts);
-    });
-  };  
-  return (
-    <>
-      <h4>Publicações</h4>
-      <div>
-        <button onClick={escutarCliqueAcessarAPI}>Atualizar publicações</button>
-      </div>
-      <ul>
-        {
-          publicacoes.map((item: Publicacao) => {
-            return <ItemPublicacao key={item.id} titulo={item.titulo} body={item.body} />
-          })
-        }
-      </ul>
-    </>
-  );
-}
-const ItemPublicacao = (props: {titulo: string; body: string}) => {
-  return (<li>{props.titulo} - {props.body}</li>);
-}
-
-const ListaDeUsuarios = () => {
-  const [usuarios, setUsuarios] = useState([]);
-  const escutarCliqueAcessarAPI = () => {
-    axios.get("https://jsonplaceholder.typicode.com/users/").then((resposta: AxiosResponse) => {
-      const users = resposta.data.map((user: { id: number; name: string; username: string; email: string;  address: object; phone: string; website: string; company: object;}) => {
-        return {
-          id: user.id,
-          nome: user.name,
-          usuario: user.username,
-          email: user.email,
-          endereco: user.address,
-          telefone: user.phone,
-          site: user.website,
-          companhia: user.company,
-        };
-      });
-      setUsuarios(users);
-    });
-  };  
-  return (
-    <>
-      <h4>Usuários</h4>
-      <div>
-        <button onClick={escutarCliqueAcessarAPI}>Atualizar usuários</button>
-      </div>
-      <ul>
-        {
-          usuarios.map((item: Usuario) => {
-            return <ItemUsuario key={item.id} name={item.name} username={item.username} email={item.email} address={item.address} phone={item.phone} website={item.website} company={item.company} />
-          })
-        }
-      </ul>
-    </>
-  );
-}
-const ItemUsuario = (props: {name: string; username: string; email: string; address: object; phone: string; website: string; company: object}) => {
-  return (<li>{props.name} - {props.username} - {props.email} - {props.address} - {props.phone} - {props.website} - {props.company}</li>);
-}
-
-const ListaDeAlbuns = () => {
-  const [albuns, setAlbuns] = useState([]);
-  const escutarCliqueAcessarAPI = () => {
-    axios.get("https://jsonplaceholder.typicode.com/albums").then((resposta: AxiosResponse) => {
-      const albuns = resposta.data.map((album: { id: number; title: string;}) => {
-        return {
-          id: album.id,
-          titulo: album.title,
-        };
-      });
-      setAlbuns(albuns);
-    });
-  };  
-  return (
-    <>
-      <h4>Albuns</h4>
-      <div>
-        <button onClick={escutarCliqueAcessarAPI}>Atualizar albuns</button>
-      </div>
-      <ul>
-        {
-          albuns.map((item: Album) => {
-            return <ItemAlbum key={item.id} titulo={item.titulo} />
-          })
-        }
-      </ul>
-    </>
-  );
-}
-const ItemAlbum = (props: {titulo: string}) => {
-  return (<li>{props.titulo}</li>);
-}
-
-
-const App = () => {
-  return (
-    <div className="avaliacao">
-      <h1>Infoweb - React</h1>
-      <ListaDeTarefas />
-      <ListaDePublicacoes />
-      <ListaDeAlbuns />
-      <ListaDeUsuarios />
-    </div>
-  );
-}
-
-export default App;*/
-
-
-
-//----------------------------------------------------------------------------------------------------------
-
-
 type Tarefa = {
   id: number,
   titulo: string;
@@ -251,9 +67,9 @@ const ListaDePublicacoes = () => {
   
   return (
     <div>
-      <h4>publicacoes</h4>
+      <h4>Publicações</h4>
       <div>
-        <button onClick={escutarpublicacoes}>Atualizar lista de tarefas</button>
+        <button onClick={escutarpublicacoes}>Atualizar lista de Publicações</button>
       </div>
       <ul>
         {
@@ -300,7 +116,7 @@ const ListaDeUsuarios = () => {
     <div>
       <h4>Usuarios</h4>
       <div>
-        <button onClick={escutaruser}>Atualizar lista de tarefas</button>
+        <button onClick={escutaruser}>Atualizar lista de Usuarios</button>
       </div>
       <ul>
         {
@@ -341,7 +157,7 @@ const ListaDeAlbuns = () => {
     <div>
       <h4>Albuns</h4>
       <div>
-        <button onClick={escutaralbuns}>Atualizar lista de tarefas</button>
+        <button onClick={escutaralbuns}>Atualizar lista de Albuns</button>
       </div>
       <ul>
         {
